34395cd498a4c75b2cc799ea87fc50dfc0b2ad12
Vanya Bondarenko <113851939+Garelkaa@users.noreply.github.com>
2023-04-03 11:31:26 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import cfg as cfg
 
-btnUrlChannel = InlineKeyboardButton(text=""#textButton, url=cfg.CHANNEL_URL)
+btnUrlChannel = InlineKeyboardButton(text="textButton", url=cfg.CHANNEL_URL)
 channelMenu = InlineKeyboardMarkup(row_width=1)
 channelMenu.insert(btnUrlChannel)
